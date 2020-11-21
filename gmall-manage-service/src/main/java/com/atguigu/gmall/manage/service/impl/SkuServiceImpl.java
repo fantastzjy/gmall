8b2d297c721809d0d9542f22f749d3ad680b45d4
fantastzjy <849372293@qq.com>
2020-11-21 19:13:05 +0800

    谷粒商城

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
@@ -0,0 +1,63 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.PmsSkuAttrValue;
+import com.atguigu.gmall.bean.PmsSkuImage;
+import com.atguigu.gmall.bean.PmsSkuInfo;
+import com.atguigu.gmall.bean.PmsSkuSaleAttrValue;
+import com.atguigu.gmall.manage.mapper.PmsSkuAttrValueMapper;
+import com.atguigu.gmall.manage.mapper.PmsSkuImageMapper;
+import com.atguigu.gmall.manage.mapper.PmsSkuInfoMapper;
+import com.atguigu.gmall.manage.mapper.PmsSkuSaleAttrValueMapper;
+import com.atguigu.gmall.service.SkuService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+
+@Service
+public class SkuServiceImpl implements SkuService {
+
+    @Autowired
+    PmsSkuInfoMapper pmsSkuInfoMapper;
+
+    @Autowired
+    PmsSkuAttrValueMapper pmsSkuAttrValueMapper;
+
+    @Autowired
+    PmsSkuSaleAttrValueMapper pmsSkuSaleAttrValueMapper;
+
+    @Autowired
+    PmsSkuImageMapper pmsSkuImageMapper;
+
+    @Override
+    public void saveSkuInfo(PmsSkuInfo pmsSkuInfo) {
+        //返回的是插入了几个值  不是返回的主键  主键是直接封装到了bean中
+        int i = pmsSkuInfoMapper.insertSelective(pmsSkuInfo);
+        //获取自增主键值
+        String skuInfoId = pmsSkuInfo.getId();
+        // 插入平台属性关联
+        List<PmsSkuAttrValue> skuAttrValueList = pmsSkuInfo.getSkuAttrValueList();
+        for (PmsSkuAttrValue pmsSkuAttrValue : skuAttrValueList) {
+            pmsSkuAttrValue.setSkuId(skuInfoId);
+            pmsSkuAttrValueMapper.insertSelective(pmsSkuAttrValue);
+        }
+        // 插入销售属性关联
+        List<PmsSkuSaleAttrValue> skuSaleAttrValueList = pmsSkuInfo.getSkuSaleAttrValueList();
+        for (PmsSkuSaleAttrValue pmsSkuSaleAttrValue : skuSaleAttrValueList) {
+            pmsSkuSaleAttrValue.setSkuId(skuInfoId);
+            pmsSkuSaleAttrValueMapper.insertSelective(pmsSkuSaleAttrValue);
+        }
+
+        // 插入图片信息
+        List<PmsSkuImage> skuImageList = pmsSkuInfo.getSkuImageList();
+        for (PmsSkuImage pmsSkuImage : skuImageList) {
+            pmsSkuImage.setSkuId(skuInfoId);
+            pmsSkuImageMapper.insertSelective(pmsSkuImage);
+        }
+
+
+    }
+}
+
+
