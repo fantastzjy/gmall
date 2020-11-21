dbb19c4d433c7b252cf6c393ac2e8738b50d4fd7
fantastzjy <849372293@qq.com>
2020-11-21 20:52:00 +0800

    谷粒商城

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
@@ -35,24 +35,26 @@ public class SkuServiceImpl implements SkuService {
         //返回的是插入了几个值  不是返回的主键  主键是直接封装到了bean中
         int i = pmsSkuInfoMapper.insertSelective(pmsSkuInfo);
         //获取自增主键值
-        String skuInfoId = pmsSkuInfo.getId();
+        String skuId = pmsSkuInfo.getId();
         // 插入平台属性关联
         List<PmsSkuAttrValue> skuAttrValueList = pmsSkuInfo.getSkuAttrValueList();
         for (PmsSkuAttrValue pmsSkuAttrValue : skuAttrValueList) {
-            pmsSkuAttrValue.setSkuId(skuInfoId);
+            pmsSkuAttrValue.setSkuId(skuId);
             pmsSkuAttrValueMapper.insertSelective(pmsSkuAttrValue);
         }
+
         // 插入销售属性关联
         List<PmsSkuSaleAttrValue> skuSaleAttrValueList = pmsSkuInfo.getSkuSaleAttrValueList();
         for (PmsSkuSaleAttrValue pmsSkuSaleAttrValue : skuSaleAttrValueList) {
-            pmsSkuSaleAttrValue.setSkuId(skuInfoId);
+            pmsSkuSaleAttrValue.setSkuId(skuId);
             pmsSkuSaleAttrValueMapper.insertSelective(pmsSkuSaleAttrValue);
         }
+
 
         // 插入图片信息
         List<PmsSkuImage> skuImageList = pmsSkuInfo.getSkuImageList();
         for (PmsSkuImage pmsSkuImage : skuImageList) {
-            pmsSkuImage.setSkuId(skuInfoId);
+            pmsSkuImage.setSkuId(skuId);
             pmsSkuImageMapper.insertSelective(pmsSkuImage);
         }
 
