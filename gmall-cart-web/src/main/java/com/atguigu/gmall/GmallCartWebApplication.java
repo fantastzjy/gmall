e10d3e733260bb9f176292679255583a2dde525b
fantastzjy <849372293@qq.com>
2022-01-05 23:18:39 +0800

    1

--- gmall-cart-web/src/main/java/com/atguigu/gmall/GmallCartWebApplication.java
+++ gmall-cart-web/src/main/java/com/atguigu/gmall/GmallCartWebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GmallCartWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GmallCartWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GmallCartWebApplication.class, args);
+    }
 
 }
